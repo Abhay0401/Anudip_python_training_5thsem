81cef6381709eaf6242ad5b9307cd21fb49827d5
Abhay Saxena <a2024cseaiml10173@imsec.ac.in>
2026-06-05 15:00:56 +0530

    Add consecutive number detector script

---
+++ classwork/list_5thjune/consecutive_no_dector.py
@@ -0,0 +1,11 @@
+numbers = [4, 5, 6, 10, 11, 15, 16, 17]
+
+consecutive_pairs = []
+
+for i in range(len(numbers) - 1):
+    if numbers[i + 1] == numbers[i] + 1:
+        print(f"{numbers[i]} and {numbers[i + 1]} are consecutive")
+        consecutive_pairs.append((numbers[i], numbers[i + 1]))
+
+print("\nConsecutive Pairs:")
+print(consecutive_pairs)
